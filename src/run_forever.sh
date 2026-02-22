8431bc363bd4b64647f3fea65d8b3e2e9d6e0346
daniellaisabella <dato0001@stud.kea.dk>
2026-02-22 15:53:53 +0100

    shellcheck on run_forever.sh

--- src/run_forever.sh
+++ src/run_forever.sh
@@ -2,12 +2,9 @@
 
 PYTHON_SCRIPT_PATH=$1
 
-TMP="This variable might become useful at some point. Otherwise delete it." 
-
-while true
+while true;
 do
-    python2 $PYTHON_SCRIPT_PATH
-    if [ $? -ne 0 ]; then
+    if ! python2 "$PYTHON_SCRIPT_PATH"; then
         echo "Script crashed with exit code $?. Restarting..." >&2
         sleep 1
     fi
