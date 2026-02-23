54740fb93fb595fcb6868cd37c22e06a6f067af3
daniellaisabella <dato0001@stud.kea.dk>
2026-02-23 13:33:49 +0100

    Refactor from Pythoon 2 to 3

--- src/run_forever.sh
+++ src/run_forever.sh
@@ -4,7 +4,7 @@ PYTHON_SCRIPT_PATH=$1
 
 while true;
 do
-    if ! python2 "$PYTHON_SCRIPT_PATH"; then
+    if ! python3 "$PYTHON_SCRIPT_PATH"; then
         echo "Script crashed with exit code $?. Restarting..." >&2
         sleep 1
     fi
